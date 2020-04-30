8c8f3852f496cc68cd75de8bbefee6e92dc67682
Vivek T V <vivekam101@gmail.com>
2020-04-30 11:54:48 +0530

    Update ner.py

--- src/ner.py
+++ src/ner.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-#    Copyright (C) 2019-TODAY Cleareye.ai(<https://www.cleareye.ai>)
-#    Author: Cleareye.ai(<https://www.cleareye.ai>)
-
 import numpy as np
 import os
 from keras.models import load_model
